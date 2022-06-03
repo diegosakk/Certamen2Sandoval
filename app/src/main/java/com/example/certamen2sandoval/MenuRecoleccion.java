df6b219adfdaba854f7437531e1bd7162bd43d6c
Diego Sandoval <diego.sandoval1@virginiogomez.cl>
2022-06-02 23:43:24 -0400

    añadidas weas de recoleccion muy basicas

---
+++ app/src/main/java/com/example/certamen2sandoval/MenuRecoleccion.java
@@ -0,0 +1,20 @@
+package com.example.certamen2sandoval;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuRecoleccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_recoleccion);
+    }
+    public void ActivityRegistrarRecoleccion(View view){
+        Intent intent = new Intent(MenuRecoleccion.this, RegistrarRecoleccion.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/certamen2sandoval/RegistrarRecoleccion.java
@@ -0,0 +1,14 @@
+package com.example.certamen2sandoval;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarRecoleccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_recoleccion);
+    }
+}
